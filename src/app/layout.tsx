4735971c4eaaf0b52dd7c09a84199d4c9803167b
Stephen Shaw <shshaw@users.noreply.github.com>
2025-06-18 09:52:12 -0700

    Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Smooch_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const smooch = Smooch_Sans({
+  variable: "--font-smooch-sans",
   subsets: ["latin"],
 });
 
@@ -24,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${smooch.variable} ${smooch.variable} antialiased`}>
         {children}
       </body>
     </html>
