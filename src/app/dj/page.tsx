81a9dea450d7967534dfc13818ac9cf5e31cbfee
David Khourshid <david.khourshid@tango.us>
2025-12-10 08:44:20 -0500

    Cleanup

--- src/app/dj/page.tsx
+++ src/app/dj/page.tsx
@@ -1,54 +1,34 @@
-"use client";
-import { useSelector } from "@xstate/store/react";
-import { appStore } from "@/app/store";
-import { shallowEqual } from "@xstate/store";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Timeline } from "./components/Timeline";
-import { WelcomeView } from "./components/WelcomeView";
-import { ExploreView } from "./components/ExploreView";
-import { SongView } from "./components/SongView";
-import Scene from "./components/Scene";
+'use client';
+import { useSelector } from '@xstate/store/react';
+import { appStore } from '@/app/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Timeline } from './components/Timeline';
+import Scene from './components/Scene';
 
-import styles from "./DJ.module.css";
-import Button from "@/components/Button";
-import { Song } from "../types";
-import { Bounds, Html } from "@react-three/drei";
-import ChatView from "./components/ChatView";
+import styles from './DJ.module.css';
+import Button from '@/components/Button';
+import ChatView from './components/ChatView';
 
 const queryClient = new QueryClient();
 
 export default function DJPage() {
   const state = useSelector(appStore, (state) => state);
-  // const currentNode = useSelector(
-  //   appStore,
-  //   (state) => {
-  //     return state.context.graph.nodes.find(
-  //       (node) => node.id === state.context.currentNodeId
-  //     );
-  //   },
-  //   shallowEqual
-  // );
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Scene>
-        {/* <Bounds>
-          <Html transform sprite={true} occlude={false}></Html>
-        </Bounds> */}
-      </Scene>
+      <Scene />
 
       <div className={styles.layout}>
         <ChatView className={styles.chatView} />
-        {/* <div className={styles.content}></div> */}
 
         <Button
           className={styles.modeToggle}
           onClick={() => appStore.trigger.toggleMode()}
         >
-          {state.context.mode === "single" ? "Show Timeline" : "Hide Timeline"}
+          {state.context.mode === 'single' ? 'Show Timeline' : 'Hide Timeline'}
         </Button>
 
-        {state.context.mode === "timeline" && (
+        {state.context.mode === 'timeline' && (
           <Timeline
             className={styles.timeline}
             nodes={state.context.graph.nodes}
